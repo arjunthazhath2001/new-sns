673d4a2970e72a8c84d365fc33ba1274ff95252b
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2024-09-24 21:39:33 +0530

    ok

--- src/components/ExpandableCardDemo1.tsx
+++ src/components/ExpandableCardDemo1.tsx
@@ -124,7 +124,7 @@ export function ExpandableCardDemo1() {
         ) : null}
       </AnimatePresence>
       <ul className="max-w-2xl mx-auto w-full gap-4">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
             key={`card-${card.title}-${id}`}
