7b09cc34ad2c530fe74761599db8a6f8cc23509e
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2024-09-25 15:23:10 +0530

    ok

--- src/components/Leader2.tsx
+++ src/components/Leader2.tsx
@@ -1,16 +1,11 @@
 "use client";
 import React from 'react';
-import Image from 'next/image';
-import { motion } from 'framer-motion'; // Import motion from Framer Motion
 import { LayoutGridDemo } from './LayoutGridDemo';
 
 export default function Leader2() {
   return (
-    <>
-      
+    <>  
         <LayoutGridDemo/>
-
-
         </>
         
     )
